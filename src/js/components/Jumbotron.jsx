673b1a8c3da5dbe5b8ca0a7d6e8d022cf72fb861
bryam <layhamshavii@gmail.com>
2025-04-23 09:55:15 +0000

    bryam-landing

--- src/js/components/Jumbotron.jsx
+++ src/js/components/Jumbotron.jsx
@@ -4,7 +4,7 @@ const Jumbotron = () => {
     return (
         <div className="mb-3 bg-primary-subtle rounded-3 mx-3 mt-4">
             <div className="conteiner-fluid py-5 px-3 text-start" >
-                <h1 className="display-5 fw-bold">A Warm Welcome!</h1>
+                <h1 className="display-4 fw-bold">A Warm Welcome!</h1>
                 <p className="col-md-8 fs-4">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore facere distinctio voluptates quia officiis unde nobis neque veniam hic. Sequi totam laudantium, velit a quaerat deleniti blanditiis facere reiciendis cumque!
                 </p>
--- src/js/components/Navbar.jsx
+++ src/js/components/Navbar.jsx
@@ -5,19 +5,19 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">Developers</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
           <ul className="navbar-nav ms-auto my-2 my-lg-0 navbar-nav-scroll align-items-start">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Inicio</a>
+              <a className="dropdown-item" href="#">Inicio</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Acerca de</a>
+              <a className="dropdown-item" href="#">Acerca de</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Contacto</a>
+              <a className="dropdown-item" href="#">Contacto</a>
             </li>
           </ul>
         </div>
